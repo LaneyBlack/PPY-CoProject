b569bf3080c8adeebbb5c75d9e7458251813c002
Palina Brahina <s24601@pjwstk.edu.pl>
2023-06-17 13:51:00 +0200

    Creating the camera: Done

--- code/entity/level.py
+++ code/entity/level.py
@@ -12,7 +12,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # Sprite Groups
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
 
         self.init_map()
@@ -27,13 +27,38 @@ class Level:
                 if key == "x":
                     Tile((x, y), [self.visible_sprites, self.obstacles_sprites])
                 elif key == "p":
-                    self.player = Player((x, y), [self.visible_sprites],self.obstacles_sprites)
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites)
 
     def run(self):
         """
-        Update and draw the game
-        :return:
+        Update and draw the game.
+        :return: None
         """
-        self.visible_sprites.draw(self.display_surface)  # draw all visible sprites on screen
+        self.visible_sprites.custom_draw(self.player)  # draw all visible sprites on screen
         self.visible_sprites.update()  # update all visible sprites on screen
         debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        """
+        Method for drawing sprites on the screen with camera offset.
+        :param player: Object representing the player,
+            whose position will be used to calculate the offset.
+        :return: None
+        """
+
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)

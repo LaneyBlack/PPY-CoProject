aa892431d275763c625362e5b4a3819050429cb2
LaneyBlack-Laptop <laney.black.74@gmail.com>
2023-06-19 12:24:30 +0200

    Path to images folder is now shared between all pathes in settings.py

--- code/entity/tile.py
+++ code/entity/tile.py
@@ -3,10 +3,9 @@ from code.settings import *
 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, sprite_type, surface=pygame.Surface((TILE_SIZE,TILE_SIZE))):
+    def __init__(self, pos, groups, sprite_type, surface=pygame.Surface((TILE_SIZE, TILE_SIZE))):
         super().__init__(groups)
         self.sprite_type = sprite_type
         self.image = surface
-        self.rect = self.image.get_rect(topleft = pos)
-        self.hitbox = self.rect.inflate(0,-10)
-
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -10)
--- code/settings.py
+++ code/settings.py
@@ -1,26 +1,30 @@
 """There you can find and change all the project settings"""
+# WARING: on Mac it is "../img", on Win it is "img"
+IMAGE_FOLDER_PATH = "img" # ../img on Mac and "img" on Win
 
 # ---===--- Main window settings ---===---
 GAME_TITLE = "Harry Potter : The Adventures"
-GAME_ICON_PATH = "../img/icon.png"
+GAME_ICON_PATH = IMAGE_FOLDER_PATH + "/icon.png"
 WIN_WIDTH = 1640  # px
 WIN_HEIGHT = 920  # px
 FPS = 60
 
 # ---===--- Texture paths ---===---
-PLAYER_TEXTURE_PATH = "../img/graphics/cat.png"
+PLAYER_TEXTURE_PATH = IMAGE_FOLDER_PATH + "/graphics/cat.png"
 PLAYER_TEXTURE_SIZE = (62, 62)
-TILE_TEXTURE_PATH = "../img/graphics/rock1.png"
+TILE_TEXTURE_PATH = IMAGE_FOLDER_PATH + "/graphics/rock1.png"
 TILE_TEXTURE_SIZE = (80, 80)
 # ---===--- Map settings ---===---
-FLOOR_TEXTURE_PATH = "../img/map/floor.png"
+FLOOR_TEXTURE_PATH = IMAGE_FOLDER_PATH + "/map/floor.png"
 FLOOR_TEXTURE_SIZE = (3200, 3200)
 
-FLOOR_BLOCKS_PATH_CSV = "../img/map/map._FloorBlocks.csv"
+FLOOR_BLOCKS_PATH_CSV = IMAGE_FOLDER_PATH + "/map/map._FloorBlocks.csv"
 
-FLOOR_OBJECTS_PATH = "../img/map/objects"
-FLOOR_OBJECTS_PATH_CSV = "../img/map/map._Objects.csv"
+FLOOR_OBJECTS_PATH = IMAGE_FOLDER_PATH + "/map/objects"
+FLOOR_OBJECTS_PATH_CSV = IMAGE_FOLDER_PATH + "/map/map._Objects.csv"
 TILE_SIZE = 64  # px
+
+# ToDo this is not needed for now
 # Currently it's 30x20 (WxH). Here in lists 20 columns and 30 rows
 # x - rock, p - player, " " - is empty space
 # WARNING! BEWARE! this map is rotated (North is in this direction <---)
